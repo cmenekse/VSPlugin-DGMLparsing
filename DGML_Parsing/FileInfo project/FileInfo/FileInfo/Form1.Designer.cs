d82792e0bc1f21122598c2b448ee1d0737f7d232
canmenekse@sabanciuniv.edu <canmenekse@sabanciuniv.edu>
2013-08-07 12:44:19 +0200

    added some files

---
+++ DGML_Parsing/FileInfo project/FileInfo/FileInfo/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace FileInfo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.browse = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Press = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.Count = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.typeSelectionBox = new System.Windows.Forms.ComboBox();
+            this.checkButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // browse
+            // 
+            this.browse.Location = new System.Drawing.Point(40, 77);
+            this.browse.Name = "browse";
+            this.browse.Size = new System.Drawing.Size(75, 23);
+            this.browse.TabIndex = 0;
+            this.browse.Text = "Browse";
+            this.browse.UseVisualStyleBackColor = true;
+            this.browse.Click += new System.EventHandler(this.browse_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(40, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(217, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Press
+            // 
+            this.Press.Location = new System.Drawing.Point(171, 77);
+            this.Press.Name = "Press";
+            this.Press.Size = new System.Drawing.Size(75, 23);
+            this.Press.TabIndex = 2;
+            this.Press.Text = "Press";
+            this.Press.UseVisualStyleBackColor = true;
+            this.Press.Click += new System.EventHandler(this.Press_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(366, 35);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(572, 458);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // Count
+            // 
+            this.Count.AutoSize = true;
+            this.Count.Location = new System.Drawing.Point(409, 12);
+            this.Count.Name = "Count";
+            this.Count.Size = new System.Drawing.Size(35, 13);
+            this.Count.TabIndex = 4;
+            this.Count.Text = "Count";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(469, 9);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // typeSelectionBox
+            // 
+            this.typeSelectionBox.FormattingEnabled = true;
+            this.typeSelectionBox.Location = new System.Drawing.Point(92, 165);
+            this.typeSelectionBox.Name = "typeSelectionBox";
+            this.typeSelectionBox.Size = new System.Drawing.Size(121, 21);
+            this.typeSelectionBox.TabIndex = 6;
+            // 
+            // checkButton
+            // 
+            this.checkButton.Location = new System.Drawing.Point(112, 217);
+            this.checkButton.Name = "checkButton";
+            this.checkButton.Size = new System.Drawing.Size(75, 23);
+            this.checkButton.TabIndex = 7;
+            this.checkButton.Text = "check";
+            this.checkButton.UseVisualStyleBackColor = true;
+            this.checkButton.Click += new System.EventHandler(this.checkButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(950, 517);
+            this.Controls.Add(this.checkButton);
+            this.Controls.Add(this.typeSelectionBox);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.Count);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.Press);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.browse);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button browse;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Press;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label Count;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox typeSelectionBox;
+        private System.Windows.Forms.Button checkButton;
+    }
+}
+
---
+++ DGML_Parsing/FileInfo project/FileInfo/FileInfo/Form1.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.IO;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace FileInfo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            typeSelectionBox.Items.Add("Assembly");
+            typeSelectionBox.Items.Add("Class");
+            typeSelectionBox.Items.Add("Namespace");
+        }
+
+        public string rawTextFile;
+
+        private void browse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.InitialDirectory = @"C:\Users\Windows 7\Documents\Visual Studio 2012\Projects\Parser\Parser\bin\Debug";
+            dialog.ShowDialog();
+            string filePath = dialog.FileName;
+            textBox1.Text = filePath;
+        }
+
+
+        public bool importFile(string filePath)
+        {
+            //Check Whether File exists
+            try
+            {
+                if (!File.Exists(filePath))
+                {
+                    throw new FileNotFoundException();
+                }
+                rawTextFile = File.ReadAllText(filePath);
+                return true;
+            }
+            catch (FileNotFoundException e)
+            {
+                return false;
+            }
+        }
+
+        private void Press_Click(object sender, EventArgs e)
+        {
+
+            SortedSet<string> set = new SortedSet<string>();
+            int Count = 0;
+            if (importFile(textBox1.Text) == true)
+            {
+
+                rawTextFile = removeWhiteSpaceMethod(rawTextFile);
+                XDocument dgmlDoc = XDocument.Parse(rawTextFile);
+                XElement root = StripNamespaces(dgmlDoc.Root);
+                XElement Aliases = root.Element("IdentifierAliases");
+                string newTextFile = "";
+                IEnumerable<XElement> singleAliases = Aliases.Descendants("Alias");
+                IEnumerable<XElement> nodesInTheFile = root.Element("Nodes").Descendants("Node");
+                Boolean[] aliasMatchedOrNot;
+                aliasMatchedOrNot = new Boolean[3370];
+                string[] aliasIdStr;
+                aliasIdStr = new string[3370];
+                string[] labels;
+                labels = new string[3370];
+                int[] mx;
+                mx = new int[3370];
+                int count = 0;
+
+                foreach (XElement node in nodesInTheFile)
+                {
+                    count++;
+                    bool found = false;
+                    string labelToSearch = node.Attribute("Label").Value;
+                    foreach (XElement alias in singleAliases)
+                    {
+                        if (alias.Attribute("Id") != null)
+                        {
+                            if (alias.Attribute("Id").Value.Contains(labelToSearch))
+                            {
+                                found = true;
+                            }
+                        }
+                    }
+                    if (found == false)
+                    {
+                        
+                        richTextBox1.AppendText(Environment.NewLine + node.Attribute("Id").Value);
+                        textBox1.Text = count.ToString();
+                    }
+                }
+            }
+        }
+
+        public static string removeWhiteSpaceMethod(string str)
+        {
+            System.Console.WriteLine("it is the remove white space1  is running ");
+            DateTime Before = DateTime.Now;
+            string asAscii = Encoding.ASCII.GetString(
+            Encoding.Convert(
+                Encoding.UTF8,
+                Encoding.GetEncoding(
+                    Encoding.ASCII.EncodingName,
+                    new EncoderReplacementFallback(string.Empty),
+                    new DecoderExceptionFallback()
+                    ),
+                Encoding.UTF8.GetBytes(str)
+            )
+        );
+            DateTime After = DateTime.Now;
+            TimeSpan difference = After - Before;
+            System.Console.WriteLine(difference.Milliseconds + "  miliseconds passed");
+            //System.Console.WriteLine("it is done ");
+            return asAscii;
+        }
+
+
+
+        private static XElement StripNamespaces(XElement rootElement)
+        {
+            foreach (var element in rootElement.DescendantsAndSelf())
+            {
+                // update element name if a namespace is available
+                if (element.Name.Namespace != XNamespace.None)
+                {
+                    element.Name = XNamespace.None.GetName(element.Name.LocalName);
+                }
+
+                // check if the element contains attributes with defined namespaces (ignore xml and empty namespaces)
+                bool hasDefinedNamespaces = element.Attributes().Any(attribute => attribute.IsNamespaceDeclaration ||
+                        (attribute.Name.Namespace != XNamespace.None && attribute.Name.Namespace != XNamespace.Xml));
+
+                if (hasDefinedNamespaces)
+                {
+                    // ignore attributes with a namespace declaration
+                    // strip namespace from attributes with defined namespaces, ignore xml / empty namespaces
+                    // xml namespace is ignored to retain the space preserve attribute
+                    var attributes = element.Attributes()
+                                            .Where(attribute => !attribute.IsNamespaceDeclaration)
+                                            .Select(attribute =>
+                                                (attribute.Name.Namespace != XNamespace.None && attribute.Name.Namespace != XNamespace.Xml) ?
+                                                    new XAttribute(XNamespace.None.GetName(attribute.Name.LocalName), attribute.Value) :
+                                                    attribute
+                                            );
+
+                    // replace with attributes result
+                    element.ReplaceAttributes(attributes);
+                }
+            }
+            return rootElement;
+        }
+
+        private void checkButton_Click(object sender, EventArgs e)
+        {
+            if (importFile(textBox1.Text) == true)
+            {
+
+                int count = 0;
+                XDocument dgmlDoc = XDocument.Parse(rawTextFile);
+                XElement root = StripNamespaces(dgmlDoc.Root);
+                XElement nodes = root.Element("Nodes");
+                IEnumerable<XElement> singleNodes = nodes.Descendants("Node");
+                foreach (XElement singleNode in singleNodes)
+                {
+                    if (singleNode.Attribute("Category").Value == "CodeSchema_" + typeSelectionBox.SelectedItem.ToString())
+                    {
+                        count++;
+                        richTextBox1.Text += Environment.NewLine + singleNode.Attribute("Id").Value + " )" + singleNode.Attribute("Label").Value;
+                    }
+                }
+                textBox1.Text = count.ToString();
+            }
+
+        }
+    }
+}
+
+/*HashSet<string> hash = new HashSet<string>();
+            
+          int count = 0;
+          if (importFile(textBox1.Text) == true)
+          {
+              string pattern=@".*?Category:(.*)";
+              string[] lines = rawTextFile.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None);
+               
+              foreach (string line in lines)
+              {
+                  Match match=Regex.Match(line,pattern);
+                  string str = match.Groups[1].ToString()+Environment.NewLine;
+                  if (!hash.Contains(str))
+                  {
+                      hash.Add(str);
+                      richTextBox1.AppendText(str);
+                      count++;
+                  }
+                    
+                    
+                    
+                    
+              }
+              textBox2.Text = count.ToString();
+          }*/
+/*
+  foreach (XElement node in nodesInTheFile)
+  {
+
+      if (node.Attribute("Uri") == null)
+      {
+          string nodeLabel = node.Attribute("Label").Value;
+
+          foreach (XElement alias in singleAliases)
+          {
+                            
+              if (alias.Attribute("Id") != null)
+              {
+
+                  if (print == true)
+                  {
+                      aliasIdStr[Convert.ToInt32(alias.Attribute("n").Value)] = alias.Attribute("Id").Value;
+                                    
+                  }
+                                
+                  string id = alias.Attribute("Id").Value;
+                  if(id.Contains (nodeLabel))
+                  {
+                      aliasIds[Convert.ToInt32(alias.Attribute("n").Value)]=true;
+                      if (nodeLabel.Length > mx[Convert.ToInt32(alias.Attribute("n").Value)]&&nodeLabel!="Parameters")
+                      {
+                          contains[Convert.ToInt32(alias.Attribute("n").Value)] = nodeLabel;
+                          mx[Convert.ToInt32(alias.Attribute("n").Value)] = nodeLabel.Length;
+                      }
+                  }
+                               
+              }
+          }
+
+
+
+                        
+
+      }
+
+      print = false;
+
+  }
+  */
+
+
+
+/*
+foreach (XElement singleAlias in singleAliases)
+{
+    string searchedValue = "@" + singleAlias.Attribute("n").Value;
+
+    IEnumerable<XElement> nodes =
+    from node in nodesInTheFile
+    where node.Attribute("Id").Value == searchedValue
+    select node;
+
+    int b = 9;
+    if (nodes.Count() == 0)
+    {
+        count++;
+                        
+        if (singleAlias.Attribute("Id") != null)
+        {
+            string id=singleAlias.Attribute("Id").Value;
+            if ((id.Contains("ParentType") || id.Contains("ArrayRank") || id.Contains("ParameterIdentifier") ||
+    id.Contains("ParamKind") || id.Contains("GenericArguments") || id.Contains("GenericParameterCount")&&!id.Contains('@')))
+            {
+                richTextBox1.AppendText(Environment.NewLine + singleAlias.Attribute("n").Value + ")" + singleAlias.Attribute("Id").Value);
+                //rawTextFile=rawTextFile.Replace("@" + singleAlias.Attribute("n").Value+ " ", "HEY!");
+                //rawTextFile = rawTextFile.Replace("@" + singleAlias.Attribute("n").Value +")", "HEY!");
+            }
+            else
+            {
+                               
+            }
+                            
+                            
+        }
+                        
+    }
+    else
+    {
+        set.Add(searchedValue);
+                        
+    }
+}
+*/
